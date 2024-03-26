6ee5f289db76b1104fd87abc76a00715c07bddfc
Jakub Kuka <jakubkuka07@gmail.com>
2024-03-26 23:06:26 +0100

    Codedex terminal 01-nevigation

---
+++ terminal/codedex-terminal01.sh
@@ -0,0 +1,30 @@
+$ echo Hi! My name is ...
+#writes the Hi! ...
+
+$ say Hi! My name is ...
+#uses speakers to read the text out loud 
+
+$pwd
+#print working directory 
+
+$cd
+#change directory
+
+$cd..
+#move backward
+
+#cd../..
+#move backward multiple directories 
+
+$ls
+#list content
+
+$cat file-name.extension
+#see inside a file - print all text bases content, can also use echo
+
+$echo hello
+#prints out hello in the terminal 
+
+#clear
+#clears out the temrinal 
+
